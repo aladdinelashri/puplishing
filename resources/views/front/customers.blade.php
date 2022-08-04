532385ad7323e23900c01a4519977cc8d08eca94
aladdinelashri <aladdin.elashri@gmail.com>
2022-08-04 09:16:52 +0200

    Update customers.blade.php

--- resources/views/front/customers.blade.php
+++ resources/views/front/customers.blade.php
@@ -4,9 +4,10 @@
             {{ __('Customers') }}
         </h2>
     </x-slot>
+    <div class="py-12">
     <x-sidenavbar1>
     </x-sidenavbar1>
-    <div class="py-12">
+
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 @livewire('customers')
